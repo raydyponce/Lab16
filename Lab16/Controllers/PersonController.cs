d247feadd62550b2c9bc89259413da79b9a8e7e2
DESKTOP-LP18JR7\Cael Ponce <raydy.ponce@tecsup.edu.pe>
2017-12-07 00:31:52 -0500

    Creacion de controladores y vistas

---
+++ Lab16/Controllers/PersonController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+using Lab16.Models;
+using System.Web.Helpers;
+using System.Collections;
+
+namespace Lab16.Controllers
+{
+    public class PersonController : Controller
+    {
+        #region Contexto
+        private SchoolEntities _contexto;
+
+        public SchoolEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new SchoolEntities();
+                return _contexto;
+            }
+        }
+        #endregion
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(Contexto.Person.ToList());
+        }
+
+        public ActionResult Reporte()
+        {
+            List<Person> listado = new List<Person>();
+            listado = Contexto.Person.ToList();
+
+            var rptviewer = new ReportViewer();
+            rptviewer.ProcessingMode = ProcessingMode.Local;
+            rptviewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) +
+                @"Reporte.rdlc";
+
+            ReportDataSource rptdatasource = new ReportDataSource("dsPersona", Listado);
+            rptviewer.LocalReport.DataSources.Add(rptdatasource);
+            rptviewer.SizeToReportContent = true;
+
+            ViewBag.ReportViewer = rptviewer;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Reporte(string FirstName)
+        {
+            List<Person> listado = new List<Person>();
+
+        }
+    }
+}
